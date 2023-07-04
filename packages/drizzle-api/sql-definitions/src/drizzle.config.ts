5a3843802524586cd37edc17ff0a55c5c75c1b2e
Sam Peterson <samllama55@gmail.com>
2023-07-04 09:06:38 +0200

    REFACTOR: Moved related to single folder package.

--- packages/drizzle-api/sql-definitions/src/drizzle.config.ts
+++ packages/drizzle-api/sql-definitions/src/drizzle.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'drizzle-kit';
 
 const config: Config = {
   schema: [
-    'packages/sql-definitions/src/lib/cities.ts',
+    'packages/drizzle-api/sql-definitions/src/lib/cities.ts',
     // 'packages/sql-definitions/src/lib/**.schema.ts'
-    'packages/sql-definitions/src/lib/users.ts',
+    'packages/drizzle-api/sql-definitions/src/lib/users.ts',
   ],
   breakpoints: true,
   out: './drizzle',
